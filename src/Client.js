2c0b655db98a060155e0c46c1fa5179ae87bba85
Serdar Aşırow <u13589@caliksoa.local>
2020-01-10 09:37:53 +0500

    v

--- src/Client.js
+++ src/Client.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import TextChatView from './text-chat/ui-components/TextChatView'
 import useWebRTC from './text-chat/webrtc/use-webrtc'
+import iceServers from './text-chat/webrtc/ice-servers'
 import usePusherSignaling from './signaling/pusher/usePusherSignaling'
 import './css/style.css'
 export default function Client ({currentUser,roomId,target,name}){
-    const {signalingMessage, sendSignalingMessage,error }= usePusherSignaling({currentUser,roomId,target,name})
-    const {} = useWebRTC({signalingMessage,sendSignalingMessage,})
+    const {signalingMessage, sendSignalingMessage,error: signalingError }= usePusherSignaling({currentUser,roomId,target,name})
+    const {	initiateOffer,
+		error : webRTCError,
+		data,
+		open,
+		sendData,} = useWebRTC({signalingMessage,sendSignalingMessage,iceServers})
 return <div className ="client">
     <TextChatView />
 </div>
