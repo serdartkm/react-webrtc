7b4c80ddbb3f47f03588f87473936f03f109c80d
Serdar Aşırow <u13589@caliksoa.local>
2020-01-10 10:19:10 +0500

    v

--- src/Demo.js
+++ src/Demo.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import Client from './Client';
+import usePusher,{ getPusherConfig } from '.';
+import ErrorMessage from './ErrorMessage'
 export default function Demo ({title}){
-    return <div className="root">
-<h1 className="demo-title">{title}</h1>
-        <div className="demo">
-        <Client />
-        <Client />
-        </div>
+    const { currentUser: gon , error:gonError } = usePusher(getPusherConfig({ userId: 'gon' }));
+    const { currentUser: kon, error:konError } = usePusher(getPusherConfig({ userId: 'kon' }));
     
-    </div>
+    if(gonError){
+        return <ErrorMessage error ={gonError}/>
+    }
+    else if (konError){
+        return <ErrorMessage error ={konError} />
+    }
+
+    else if (gon && kon){
+        return <div className="root">
+        <h1 className="demo-title">{title}</h1>
+                <div className="demo">
+                <Client currentUser={gon} name="gon" target="kon" />
+                <Client currentUser={kon} name="kon" target="gon" />
+                </div>
+            </div>
+    }
+    return <ErrorMessage error ={{message:"Unexpected error"}} />
+
+  
 }
