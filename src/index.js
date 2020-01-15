1f94a557276279ec8bd7fefb5ca87cbee9d6388b
Serdar Aşırow <u13589@caliksoa.local>
2020-01-15 14:20:56 +0500

    v

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ ReactDOM.render(
   <Demo title="Demo Title" clientOneName="gon" clientTwoName="kon" />,
   document.getElementById("root")
 );
-
+//myoush
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
